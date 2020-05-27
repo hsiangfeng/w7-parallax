91c3c37b2d75b985e96f4ef47a6de06d65549c3d
Ray <loveyou520babytw@gmail.com>
2020-05-27 11:18:06 +0800

    Update 2020-05-27T03:17:46.095Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,11 +1,11 @@
+"use strict";
 
-$(document).ready(() => {
-  setTimeout(() => {
+$(document).ready(function () {
+  setTimeout(function () {
     $('#navbar').addClass('active');
     $('#header').addClass('active');
   }, 800);
-
-  const gearIO = new IntersectionObserver((entries) => {
+  var gearIO = new IntersectionObserver(function (entries) {
     if (entries[0].intersectionRatio >= 0.5) {
       $('#gear').addClass('active');
     }
@@ -15,15 +15,13 @@ $(document).ready(() => {
       $('.gear__content__subtitle').removeClass('activeStripped');
     }
   }, {
-    threshold: [0.5, 0.9],
+    threshold: [0.5, 0.9]
   });
   gearIO.observe($('#gear')[0]);
-
-  const strippedIO = new IntersectionObserver((entries) => {
+  var strippedIO = new IntersectionObserver(function (entries) {
     if (entries[0].intersectionRatio >= 0.14) {
       $('.gear__content__title').addClass('activeStripped');
       $('.gear__content__subtitle').addClass('activeStripped');
-
       $('#stripped').addClass('active');
     }
 
@@ -32,59 +30,59 @@ $(document).ready(() => {
       $('.stripped__content__subtitle').removeClass('activeStripped');
     }
   }, {
-    threshold: [0.14, 0.9],
+    threshold: [0.14, 0.9]
   });
   strippedIO.observe($('#stripped')[0]);
-
-  const collectionIO = new IntersectionObserver((entries) => {
+  var collectionIO = new IntersectionObserver(function (entries) {
     if (entries[0].intersectionRatio >= 0.15) {
       $('.stripped__content__title').addClass('activeStripped');
       $('.stripped__content__subtitle').addClass('activeStripped');
     }
 
-    if(entries[0].intersectionRatio >= 0.25) {
+    if (entries[0].intersectionRatio >= 0.25) {
       $('.collection__main h4').addClass('active');
       $('.collection__main p').addClass('active');
     }
   }, {
-    threshold: [0.15, 0.25],
+    threshold: [0.15, 0.25]
   });
   collectionIO.observe($('#collection')[0]);
-
-  const fixieIO = new IntersectionObserver((entries) => {
+  var fixieIO = new IntersectionObserver(function (entries) {
     if (entries[0].intersectionRatio >= 0.7) {
       $('.fixie__card').addClass('active');
     }
   }, {
-    threshold: [0.7],
+    threshold: [0.7]
   });
   fixieIO.observe($('#fixie')[0]);
-
-  const footerIO = new IntersectionObserver((entries) => {
+  var footerIO = new IntersectionObserver(function (entries) {
     if (entries[0].intersectionRatio >= 0.9) {
       $('#footer').addClass('active');
     }
   }, {
-    threshold: [0.9],
+    threshold: [0.9]
   });
   footerIO.observe($('#footer')[0]);
+  $(window).scroll(function () {
+    var scrollPos = $(this).scrollTop();
+    var windowHeight = $(window).height();
+    var gearbg = $('.gear__read-bg').offset().top;
 
-  $(window).scroll(function() {
-    const scrollPos = $(this).scrollTop();
-    const windowHeight = $(window).height();
-    const gearbg = $('.gear__read-bg').offset().top;
-    if((scrollPos + windowHeight / 2) >= gearbg) {
-      $('.gear__read-bg').css('background-position', `-${parseInt(scrollPos / 6)}px center`);
+    if (scrollPos + windowHeight / 2 >= gearbg) {
+      $('.gear__read-bg').css('background-position', "-".concat(parseInt(scrollPos / 6), "px center"));
     }
 
-    const stripped = $('#stripped').offset().top;
-    if((scrollPos + windowHeight / 2) >= stripped) {
-      $('.stripped__read-bg').css('background-position', `-${parseInt(scrollPos / 6)}px center`);
+    var stripped = $('#stripped').offset().top;
+
+    if (scrollPos + windowHeight / 2 >= stripped) {
+      $('.stripped__read-bg').css('background-position', "-".concat(parseInt(scrollPos / 6), "px center"));
     }
 
-    const collection = $('.collection__bg').offset().top;
-    if(collection >= scrollPos || collection <= scrollPos) {
-      $('.collection__bg').css('background-position', `center -${parseInt(scrollPos / 5)}px`);
+    var collection = $('.collection__bg').offset().top;
+
+    if (collection >= scrollPos || collection <= scrollPos) {
+      $('.collection__bg').css('background-position', "center -".concat(parseInt(scrollPos / 5), "px"));
     }
   });
 });
+//# sourceMappingURL=all.js.map
